4f37942fa370590ef380e53a91df1fa6147e9867
makasami <79604808+makasami@users.noreply.github.com>
2023-09-28 13:28:31 -0600

    Create Pruebita.c

---
+++ Pruebita.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // Comentarios
+
+int main (){
+    int elem1 = 1;
+    int elem2 = 2;
+    int result = suma(elem1,elem2);
+    printf("La sumita es: %d", result);
+}
+
+int suma (int op1, int op2){ // Funcion/  (Recibe 2 argumentos, )
+    return(op1+op2); // Suma
+}
